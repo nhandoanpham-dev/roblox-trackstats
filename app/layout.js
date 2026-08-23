6e4b9ee25e228d51a3a1a53a11f6e09484a17e8e
nhandoanpham-dev <nhandoanpham@gmail.com>
2026-08-23 13:28:46 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'UdumXBot',
-  description: 'Trạng Thái Máy Chủ',
+  description: 'Quản Lý Tài Khoản Roblox Của Bạn',
 };
 
 export default function RootLayout({ children }) {
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
       <head>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body className="bg-slate-950 text-slate-100 min-h-screen antialiased">
+      <body className="bg-slate-950 text-slate-100 min-h-screen antialiased selection:bg-amber-500 selection:text-slate-950">
         {children}
       </body>
     </html>
